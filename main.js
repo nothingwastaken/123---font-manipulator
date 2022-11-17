991e10612606ece1d0eac8f1524f1578adf3781b
nothingwastaken <82754118+nothingwastaken@users.noreply.github.com>
2022-11-17 18:19:48 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+lw = 0;
+rw = 0;
+dif = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550,500);
@@ -6,6 +10,14 @@ function setup(){
     canvas.position(680, 150);
 
     poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    background("b.jpg");
+    textSize(dif);
+    fill("#00EE6E");
+    text("Nothingwastaken", 50, 200);
 }
 
 function modelLoaded(){
@@ -15,5 +27,10 @@ function modelLoaded(){
 function gotPoses(results){
     if (results.length > 0){
         console.log(results)
+        lw = results[0].pose.leftWrist.x;
+        rw = results[0].pose.rightWrist.x;
+        dif = floor(lw-rw)/2;
+        textSize(dif);
+        console.log("yes")
     }
 }
